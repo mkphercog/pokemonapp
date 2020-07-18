255be1260064f06e2b2b275262056dee3138c599
Marcin Hercog <mkp.hercog@gmail.com>
2020-07-18 21:12:55 +0200

    Updates

---
+++ precache-manifest.b90a67aaea812e1f98c9ade761902169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fdaace1dd963c07dce930f1f7ec2a0",
+    "url": "/pokemonapp/index.html"
+  },
+  {
+    "revision": "f7ec43952361d98a7002",
+    "url": "/pokemonapp/static/js/2.1c9580f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemonapp/static/js/2.1c9580f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a60f468d0c516e2b10",
+    "url": "/pokemonapp/static/js/main.95a43206.chunk.js"
+  },
+  {
+    "revision": "d8e1555b16a0e3431fe7",
+    "url": "/pokemonapp/static/js/runtime-main.19a3eec6.js"
+  },
+  {
+    "revision": "956f3234a1c6154bd002c9e6a0f2f91a",
+    "url": "/pokemonapp/static/media/pokemon-logo.956f3234.png"
+  },
+  {
+    "revision": "904ba24fec8381fcf69c1a3559e067ee",
+    "url": "/pokemonapp/static/media/questionMark.904ba24f.png"
+  }
+]);
